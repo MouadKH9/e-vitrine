56df5706cdf0a5daf1345d8480f513c753f33fc7
MouadKH9 <mouad.khchich@gmail.com>
2020-05-30 17:33:13 +0000

    Popular products

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Produit;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function home()
     {
-        return view('frontend.home');
+        $topProducts = Produit::orderBy("views", 'desc')->limit(4)->get();
+        return view('frontend.home', ["popProduits" => $topProducts]);
     }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -10,6 +10,23 @@
     </h4>
 </div>
 
+<div class="container">
+    <h3 class="text-center">Produits populaires</h3>
+    <div class="row">
+        @foreach($popProduits as $prod)
+        <div class="col-md-3 prod">
+            <img src="/storage/{{ $prod->image }}" alt="Image" class="img-fluid">
+            <h4>
+                {{$prod->name}}
+            </h4>
+            <p>
+                {{$prod->description}}
+            </p>
+        </div>
+        @endforeach
+    </div>
+</div>
+
 <style>
     .wallpaper {
         height: 60vh;
@@ -28,6 +45,17 @@
     .wallpaper h4 {
         text-align: center;
         color: #dadada;
+    }
+
+    .prod img {
+        width: 100%;
+        height: 200px
+    }
+
+    .prod p {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
     }
 </style>
 
